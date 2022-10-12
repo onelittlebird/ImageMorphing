42d6f838949fa3c9a9e094409698a80a17d67f34
Filip Moberg <filip@onelittlebird.io>
2022-10-12 21:35:22 +0200

    feat: Adds watchOS platform compatibility

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ImageMorphing",
-    platforms: [.iOS(.v15), .macOS(.v12), .tvOS(.v15)],
+    platforms: [.iOS(.v15), .macOS(.v12), .tvOS(.v15), .watchOS(.v9)],
     products: [
         .library(
             name: "ImageMorphing",
